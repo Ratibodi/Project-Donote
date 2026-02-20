d247da1c77fdf95ec5246d7d533308267fc67dd7
inthitanan <inthitanan2548@gmail.com>
2026-02-21 03:12:49 +0700

    updateloginv.2

--- src/app/user/login/page.jsx
+++ src/app/user/login/page.jsx
@@ -9,7 +9,7 @@ export default function login(){
     return(
         <div className="bg-[#bae5f4] min-h-screen flex items-center justify-center">
             <div className='flex flex-col items-center justify-center w-[750px] h-[550px] bg-white border rounded-2xl'>
-                <img src="/icon/logo.png" className='w-[180px] h-[175px]'/>
+                <img src="/icon/logo.png" className='w-[150px] h-[150px]'/>
                     <div className='group flex flex-col'>
                         <span className='text-[13px] font-bold'>อีเมล</span>
                         <input
@@ -26,8 +26,6 @@ export default function login(){
                                 outline-none
                                 text-sm
                                 placeholder-gray-500
-                                focus:border-indigo-600
-                                focus:ring-2
                                 focus:ring-indigo-200
                             "
                         />
@@ -35,7 +33,7 @@ export default function login(){
                     <div className='group flex flex-col mt-8'>
                         <span className='text-[13px] font-bold'>รหัสผ่าน</span>
                         <input
-                            type="email"
+                            type="password"
                             placeholder="รหัสผ่าน"
                             className="
                                 w-[500px]
@@ -47,12 +45,14 @@ export default function login(){
                                 outline-none
                                 text-sm
                                 placeholder-gray-500
-                                focus:border-indigo-600
-                                focus:ring-2
                                 focus:ring-indigo-200
                             "
                         />
                     </div>
+                
+                    <span className='text-[13px] font-bold text-[#131376] underline decoration-[#131376] self-end px-[150px] mt-2 cursor-pointer'>ลืมรหัสผ่าน</span>
+                    <button className='w-[200px] h-[40px] bg-[#131376] rounded-2xl text-white text-[14px] font-bold mt-[70px]'>เข้าสู่ระบบ</button>
+                    <span className='text-[12px] font-bold text-black mt-2'>ยังไม่มีบัญชี? <span className='text-[#131376] underline decoration-[#131376] mt-2 cursor-pointer'>สมัครสมาชิก</span></span>
             </div>
         </div>
     )
