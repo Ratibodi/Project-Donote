006d9c928895c7139b9eb5675ce830a0e439803b
inthitanan <inthitanan2548@gmail.com>
2026-02-21 00:10:11 +0700

    createfile

--- src/app/user/login/page.jsx
+++ src/app/user/login/page.jsx
@@ -10,10 +10,10 @@ export default function login(){
         <div className="bg-[#bae5f4] min-h-screen flex items-center justify-center">
             <div className='flex flex-col items-center justify-center w-[750px] h-[550px] bg-white border rounded-2xl'>
                 <img src="/icon/logo.png" className='w-[180px] h-[175px]'/>
-                <div className='flex gap-3'>
                     <div className='group flex flex-col'>
                         <span className='text-[13px] font-bold'>อีเมล</span>
                         <input
+                        
                             type="email"
                             placeholder="อีเมล"
                             className="
@@ -31,11 +31,12 @@ export default function login(){
                                 focus:ring-indigo-200
                             "
                         />
-
+                    </div>
+                    <div className='group flex flex-col mt-8'>
                         <span className='text-[13px] font-bold'>รหัสผ่าน</span>
                         <input
                             type="email"
-                            placeholder="อีเมล"
+                            placeholder="รหัสผ่าน"
                             className="
                                 w-[500px]
                                 h-[48px]
@@ -51,11 +52,7 @@ export default function login(){
                                 focus:ring-indigo-200
                             "
                         />
-
                     </div>
-                    
-                    
-                </div>
             </div>
         </div>
     )
