3737c53ac2c633661f36a1b01a7c9cf6a8b82569
Ratibodi <ratibodee02@gmail.com>
2026-02-28 17:54:37 +0700

    เเก้4

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,6 +1,6 @@
 
-import Navbar from "../../../components/Navbar";
-import Sidebar  from "../../../components/Sidebar";
+import Navbar from "../../components/Navbar";
+import Sidebar  from "../../components/Sidebar";
 
 export const metadata = {
   title: "Donote",
